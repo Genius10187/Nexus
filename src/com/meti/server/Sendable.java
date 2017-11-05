f27d02353121281d75f132d85b6daa3e9315552f
Mathhman <minecraftmathhman@gmail.com>
2017-11-05 18:31:57 -0500

    Initial commit

--- src/com/meti/server/Sendable.java
+++
@@ -1,9 +0,0 @@
-package com.meti.server;
-
-import java.io.IOException;
-import java.io.Serializable;
-
-public interface Sendable {
-    Serializable receive() throws IOException, ClassNotFoundException;
-    void send(Serializable serializable, boolean flush) throws IOException;
-}
