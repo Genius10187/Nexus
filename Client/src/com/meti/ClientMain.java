58954d9bef6c240c2f6c3af4a20d616e60012267
Mathhman <minecraftmathhman@gmail.com>
2017-11-05 19:01:43 -0500

    Need to brush up on my concurrency...

---
+++ Client/src/com/meti/ClientMain.java
@@ -0,0 +1,20 @@
+package com.meti;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 11/5/2017
+ */
+public class ClientMain extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ Server/src/com/meti/ServerManager.java
@@ -0,0 +1,9 @@
+package com.meti;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 11/5/2017
+ */
+public class ServerManager {
+}
