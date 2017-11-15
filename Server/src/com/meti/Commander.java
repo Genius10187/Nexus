03ea54e0cd515c8a4182aa0414f141aee3e31908
Genius10187 <33701741+Genius10187@users.noreply.github.com>
2017-11-15 16:19:45 -0500

    A change.

--- Server/src/com/meti/Commander.java
+++ Server/src/com/meti/Commander.java
@@ -6,5 +6,6 @@ import com.meti.io.Command;
 import java.io.IOException;
 
 public interface Commander {
+    //i have no idea what I'm doing here
     void run(Command command, Client client) throws IOException;
 }
