60373a2b48d1c7888f921de942b14f1c49374e33
Mathhman <minecraftmathhman@gmail.com>
2017-11-30 16:27:42 -0500

    Prerequisite files

---
+++ src/com/meti/server/ServerMain.java
@@ -0,0 +1,26 @@
+package com.meti.server;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 11/30/2017
+ */
+public class ServerMain {
+    private static boolean running = false;
+
+    public static void main(String[] args) {
+        init();
+        run();
+    }
+
+    private static void init() {
+
+    }
+
+    private static void run() {
+        running = true;
+        while (running) {
+
+        }
+    }
+}
