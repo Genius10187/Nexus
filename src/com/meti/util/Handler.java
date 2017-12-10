68876804d5cf0787e6eefcd2899b1123b8214fc9
ivanvinski <ivan.vinski.dev@gmail.com>
2017-12-10 16:00:29 +0100

    Remove residual files
    For some reason I made an error and didn't commit deletion of files.
    These files aren't really deleted, they have been moved when changing
    the project structure.

--- src/com/meti/util/Handler.java
+++
@@ -1,9 +0,0 @@
-package com.meti.util;
-
-/**
- * @author SirMathhman
- * @version 0.0.0
- * @since 11/30/2017
- */
-public interface Handler<T> extends Action<T> {
-}
--- src/main/com/meti/asset/ByteAsset.java
+++
@@ -1,7 +0,0 @@
-package com.meti.asset;
-
-public class ByteAsset extends Asset {
-    public ByteAsset(int[] content) {
-        super(content);
-    }
-}
