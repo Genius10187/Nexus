a0566472befbd26bfa88a4c84a9f88d2e9e918bd
SirMathhman <minecraftmathhman@gmail.com>
2017-12-13 15:46:24 -0500

    Okay, nothing has broken. Bien.

--- src/main/java/com/meti/server/ServerCreator.java
+++ src/main/java/com/meti/server/ServerCreator.java
@@ -1,16 +1,16 @@
 package com.meti.server;
 
 import com.meti.util.Dialog;
+import com.meti.util.Utility;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.TextField;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.URL;
 import java.util.logging.Level;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.TextField;
-import javafx.stage.Stage;
 
 /**
  * @author SirMathhman
@@ -19,84 +19,84 @@ import javafx.stage.Stage;
  */
 public class ServerCreator {
 
-  private static final URL serverDisplayFXMLLocation = ServerCreator.class
-      .getResource("/fxml/ServerDisplay.fxml");
+    private static final URL serverDisplayFXMLLocation = ServerCreator.class
+            .getResource("/fxml/ServerDisplay.fxml");
 
-  @FXML
-  private TextField portField;
+    @FXML
+    private TextField portField;
 
-  private ServerSocket serverSocket;
+    private ServerSocket serverSocket;
 
-  @FXML
-  public void host() {
-    try {
-      if (serverSocket == null) {
-        serverSocket = new ServerSocket(Integer.parseInt(portField.getText()));
-      }
-
-      FXMLLoader loader = new FXMLLoader(serverDisplayFXMLLocation);
-      Parent parent = loader.load();
-      ServerDisplay display = loader.getController();
-
-      Scene scene = new Scene(parent);
-      Stage stage = new Stage();
-      display.setStage(stage);
-      stage.setScene(scene);
-      stage.show();
-
-      Server server = new Server(serverSocket);
-
-      display.setServer(server);
-      display.buildListeners();
-
-      ServerThread serverThread = new ServerThread(server);
-      server.getExecutorService().submit(serverThread);
-    } catch (NumberFormatException e) {
-      try {
-        Dialog.loadDialog().setMessage("Invalid port!");
-      } catch (IOException e1) {
-        ServerMain.log(Level.SEVERE, e1);
-      }
-    } catch (IOException e) {
-      try {
-        Dialog.loadDialog().setException(e);
-      } catch (IOException e1) {
-        ServerMain.log(Level.SEVERE, e1);
-      }
-    }
-  }
-
-  @FXML
-  public void generateLocalPort() {
-    try {
-      this.serverSocket = new ServerSocket(0);
-
-      portField.setText(String.valueOf(serverSocket.getLocalPort()));
-    } catch (IOException e) {
-      try {
-        Dialog.loadDialog().setException(e);
-      } catch (IOException e1) {
-        ServerMain.log(Level.SEVERE, e1);
-      }
-    }
-  }
-
-  @FXML
-  public void openAdvancedSettings() {
-    //TODO: advanced settings
-  }
-
-  private class ServerThread implements Runnable {
-
-    private final Server server;
-
-    public ServerThread(Server server) {
-      this.server = server;
+    @FXML
+    public void host() {
+        try {
+            createServer();
+        } catch (NumberFormatException e) {
+            try {
+                Dialog.loadDialog().setMessage("Invalid port!");
+            } catch (IOException e1) {
+                ServerMain.log(Level.SEVERE, e1);
+            }
+        } catch (IOException e) {
+            try {
+                Dialog.loadDialog().setException(e);
+            } catch (IOException e1) {
+                ServerMain.log(Level.SEVERE, e1);
+            }
+        }
     }
 
-    @Override
-    public void run() {
-      server.run();
+    private void createServer() throws IOException {
+        if (serverSocket == null) {
+            serverSocket = new ServerSocket(Integer.parseInt(portField.getText()));
+        }
+
+        FXMLLoader loader = new FXMLLoader(serverDisplayFXMLLocation);
+        Parent parent = loader.load();
+        ServerDisplay display = loader.getController();
+
+        Utility.buildStage(parent);
+
+        Server server = new Server(serverSocket);
+
+        display.setServer(server);
+        display.buildListeners();
+
+        ServerThread serverThread = new ServerThread(server);
+        server.getExecutorService().submit(serverThread);
     }
-  }
+
+    @FXML
+    public void generateLocalPort() {
+        try {
+            this.serverSocket = new ServerSocket(0);
+
+            portField.setText(String.valueOf(serverSocket.getLocalPort()));
+        } catch (IOException e) {
+            try {
+                Dialog.loadDialog().setException(e);
+            } catch (IOException e1) {
+                ServerMain.log(Level.SEVERE, e1);
+            }
+        }
+    }
+
+    @FXML
+    public void openAdvancedSettings() {
+        //TODO: advanced settings
+    }
+
+    private class ServerThread implements Runnable {
+
+        private final Server server;
+
+        public ServerThread(Server server) {
+            this.server = server;
+        }
+
+        @Override
+        public void run() {
+            server.run();
+        }
+    }
 }
