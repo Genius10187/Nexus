eca02cacd85b7eb473ced095486d8f59ec0aa8dc
Mathhman <minecraftmathhman@gmail.com>
2017-12-27 21:01:55 -0500

    Added the JavaFX testing environment

--- src/test/java/com/meti/lib/util/UtilityTest.java
+++ src/test/java/com/meti/lib/util/UtilityTest.java
@@ -16,7 +16,6 @@ import static org.junit.jupiter.api.Assertions.*;
  * @since 12/26/2017
  */
 class UtilityTest {
-
     @Test
     void toCharObjectArray() {
         char[] test = new char[]{'a', 'b', 'c'};
@@ -71,63 +70,5 @@ class UtilityTest {
         Utility.assertNullParameters("1", 2);
 
         assertThrows(NullPointerException.class, () -> Utility.assertNullParameters(null, "test"));
-    }
-
-    @Test
-    void load() {
-        //can't test FX in non-FX thread...
-        /*String code = "" +
-                "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-                "\n" +
-                "<?import javafx.scene.layout.*?>\n" +
-                "\n" +
-                "<AnchorPane xmlns=\"http://javafx.com/javafx\"\n" +
-                "            xmlns:fx=\"http://javafx.com/fxml\"\n" +
-                "            fx:controller=\"com.meti.UtilityTest.TestController\"\n" +
-                "            prefHeight=\"400.0\" prefWidth=\"600.0\">\n" +
-                "\n" +
-                "</AnchorPane>\n";
-
-        PipedInputStream pipedInputStream = new PipedInputStream();
-        PipedOutputStream pipedOutputStream = new PipedOutputStream();
-        pipedOutputStream.connect(pipedInputStream);
-
-        PrintWriter writer = new PrintWriter(pipedOutputStream);
-        writer.print(code);
-        writer.flush();
-
-        Source<InputStream, OutputStream> source = Sources.createBasicSource(pipedInputStream, pipedOutputStream);
-        Stage stage = new Stage();
-        FXMLBundle<?> bundle = Utility.load(source, stage, EnumSet.of(Utility.FXML.LOAD_STAGE));
-
-        assertNotNull(bundle);
-        assertNotNull(bundle.getController());
-        assertTrue(TestController.class.isAssignableFrom(bundle.getController().getClass()));
-
-        assertTrue(stage.isShowing());
-
-        stage.close();
-
-        pipedInputStream.close();
-        pipedOutputStream.close();*/
-    }
-
-    @Test
-    void load1() {
-    }
-
-    @Test
-    void load2() {
-    }
-
-    @Test
-    void load3() {
-    }
-
-    @Test
-    void load4() {
-    }
-
-    public class TestController {
     }
 }
