1a8ffe5e55b180d9937fdaa8e3800bd51a57b966
Mathhman <minecraftmathhman@gmail.com>
2018-01-14 14:03:55 -0500

    Set up basic files.

---
+++ src/main/java/com/meti/FXMLUtil.java
@@ -0,0 +1,25 @@
+package com.meti;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
+import java.io.IOException;
+import java.net.URL;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 1/14/2018
+ */
+public class FXMLUtil {
+    private FXMLUtil() {
+    }
+
+    //methods
+    public static <T> FXMLBundle<T> load(URL url) throws IOException {
+        FXMLLoader loader = new FXMLLoader(url);
+        Parent parent = loader.load();
+        T controller = loader.getController();
+        return new FXMLBundle<>(url, parent, controller);
+    }
+}
--- src/main/java/com/meti/Main.java
+++ src/main/java/com/meti/Main.java
@@ -1,12 +1,30 @@
 package com.meti;
 
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 1/14/2018
  */
-public class Main {
+public class Main extends Application {
+    private static final URL displayFXML = Main.class.getResource("Display.fxml");
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent parent = FXMLUtil.load(displayFXML).getParent();
+        Scene scene = new Scene(parent);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
     //methods
     public static void main(String[] args) {
+        launch(args);
     }
 }
