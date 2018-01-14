8296e60211cdf430c682a1acb772d36b526a05ac
Mathhman <minecraftmathhman@gmail.com>
2018-01-14 13:41:12 -0500

    Created the Main class

---
+++ src/main/java/com/meti/Main.java
@@ -0,0 +1,12 @@
+package com.meti;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 1/14/2018
+ */
+public class Main {
+    //methods
+    public static void main(String[] args) {
+    }
+}
