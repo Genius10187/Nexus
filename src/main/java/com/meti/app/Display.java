a9757da1a9884103951dd1a11f08d60c73b17a71
Mathhman <minecraftmathhman@gmail.com>
2018-01-15 14:59:12 -0500

    Confirmed that the windows display.

--- src/main/java/com/meti/app/Display.java
+++ src/main/java/com/meti/app/Display.java
@@ -5,7 +5,9 @@ import com.meti.util.fxml.FXUtil;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
@@ -17,6 +19,7 @@ import java.util.logging.Level;
  * @since 1/14/2018
  */
 public class Display extends Controller {
+    private final URL connectionControllerFXML = getClass().getResource("ConnectionController.fxml");
     private final URL chatFXML = getClass().getResource("Chat.fxml");
 
     @FXML
@@ -31,6 +34,18 @@ public class Display extends Controller {
             //TODO: multiple places instead of BorderPane.setCenter(Node node)
         } catch (IOException e) {
             console.log(Level.SEVERE, "Failed to load view", e);
+        }
+    }
+
+    @FXML
+    public void openConnections() {
+        try {
+            Parent parent = FXUtil.load(connectionControllerFXML).getParent();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(parent));
+            stage.show();
+        } catch (IOException e) {
+            console.log(Level.SEVERE, e);
         }
     }
 
