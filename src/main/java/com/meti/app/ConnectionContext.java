c76132b2cc34e5eaa64236a61bdbd065a2cbb51b
Mathhman <minecraftmathhman@gmail.com>
2018-01-15 14:00:25 -0500

    Added Block2P to the classpath.

--- src/main/java/com/meti/app/ConnectionContext.java
+++ src/main/java/com/meti/app/ConnectionContext.java
@@ -1,9 +1,21 @@
 package com.meti.app;
 
+import com.meti.io.connect.ConnectionHandler;
+import com.meti.io.connect.connections.Connection;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 1/15/2018
  */
 public class ConnectionContext {
+    private final ConnectionHandler handler = new ConnectionHandler() {
+        @Override
+        public Object handleThrows(Connection connection) {
+            return null;
+        }
+    };
+
+    public ConnectionContext() {
+    }
 }
