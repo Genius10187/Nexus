5867800a02586676856c21999cc2496c2275c3e9
SirMathhman <minecraftmathhman@gmail.com>
2018-01-17 15:46:55 -0500

    Finishing touches.

--- src/main/java/com/meti/app/ConnectionContext.java
+++ src/main/java/com/meti/app/ConnectionContext.java
@@ -4,13 +4,19 @@ import com.meti.io.Peer;
 import com.meti.io.connect.ConnectionHandler;
 import com.meti.io.connect.connections.Connection;
 
+import java.util.concurrent.ExecutorService;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 1/15/2018
  */
 public class ConnectionContext {
-    private final Peer peer = new Peer(new NexusConnectionHandler());
+    private final Peer peer;
+
+    public ConnectionContext(ExecutorService service) {
+        peer = new Peer(new NexusConnectionHandler(), service);
+    }
 
     public Peer getPeer() {
         return peer;
