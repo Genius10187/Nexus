aa998b303fdc2a94f856bc19b78e1efdc96d20f3
Mathhman <minecraftmathhman@gmail.com>
2018-01-21 16:53:54 -0500

    Fixed bug in NeWConnection which caused the GUI thread to wait...

--- src/main/java/com/meti/app/NewConnection.java
+++ src/main/java/com/meti/app/NewConnection.java
@@ -2,7 +2,10 @@ package com.meti.app;
 
 import com.meti.io.Sources;
 import com.meti.io.connect.connections.ObjectConnection;
+import com.meti.util.Loop;
 import javafx.animation.AnimationTimer;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -30,85 +33,114 @@ public class NewConnection implements Initializable {
     private TextField portField;
 
     @FXML
-    private Text status;
+    private Text statusText;
 
     @FXML
     private Button nextButton;
     private AnimationTimer timer;
     private ObjectConnection connection;
 
+    private String status;
+    private boolean next;
+    private Loop loop;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        NewConnectionChangeListener listener = new NewConnectionChangeListener();
+        addressField.textProperty().addListener(listener);
+        portField.textProperty().addListener(listener);
+        nextButton.setDisable(true);
+
         timer = new AnimationTimer() {
             @Override
             public void handle(long now) {
-                String addressToken = addressField.getText();
-                String portToken = portField.getText();
-                if (addressToken != null && portToken != null) {
-                    StringBuilder statusBuilder = new StringBuilder();
-                    boolean addressValid = true;
-                    boolean portValid = true;
+                statusText.setText(status);
+            }
+        };
+        timer.start();
 
-                    InetAddress address = null;
-                    int port = 0;
+        loop = new Loop() {
+            @Override
+            protected void loop() {
+                if (next) {
+                    String addressToken = addressField.getText();
+                    String portToken = portField.getText();
+                    if (addressToken != null && portToken != null) {
+                        StringBuilder statusBuilder = new StringBuilder();
+                        boolean addressValid = true;
+                        boolean portValid = true;
 
-                    try {
-                        address = InetAddress.getByName(addressToken);
-                    } catch (UnknownHostException e) {
-                        addressValid = false;
-                    }
+                        InetAddress address1 = null;
+                        int port1 = 0;
 
-                    try {
-                        port = Integer.parseInt(portToken);
-                    } catch (NumberFormatException e) {
-                        portValid = false;
-                    }
-
-                    if (addressValid || portValid) {
-                        if (!addressValid) {
-                            statusBuilder.append("Invalid address!");
-                            nextButton.setDisable(true);
+                        try {
+                            address1 = InetAddress.getByName(addressToken);
+                        } catch (UnknownHostException e) {
+                            addressValid = false;
                         }
 
-                        if (!portValid) {
-                            statusBuilder.append("Invalid port!");
-                            nextButton.setDisable(true);
+                        try {
+                            port1 = Integer.parseInt(portToken);
+                        } catch (NumberFormatException e) {
+                            portValid = false;
                         }
 
-                        if (addressValid && portValid) {
-                            try {
-                                openConnection(address, port);
-                                nextButton.setDisable(false);
-                            } catch (IOException e) {
-                                statusBuilder.append("Failed to open connection!");
+                        if (addressValid || portValid) {
+                            if (!addressValid) {
+                                statusBuilder.append("Invalid address!");
                                 nextButton.setDisable(true);
                             }
+
+                            if (!portValid) {
+                                statusBuilder.append("Invalid port!");
+                                nextButton.setDisable(true);
+                            }
+
+                            if (addressValid && portValid) {
+                                try {
+                                    openConnection(address1, port1);
+                                    nextButton.setDisable(false);
+                                } catch (IOException e) {
+                                    statusBuilder.append("Failed to open connection!");
+                                    nextButton.setDisable(true);
+                                }
+                            }
+                        } else {
+                            statusBuilder.append("Invalid address and port!");
+                            nextButton.setDisable(true);
                         }
-                    } else {
-                        statusBuilder.append("Invalid address and port!");
-                        nextButton.setDisable(true);
+
+                        status = statusBuilder.toString();
                     }
 
-                    status.setText(statusBuilder.toString());
+                    next = false;
                 }
             }
         };
 
-        timer.start();
+        appState.getService().submit(loop);
     }
 
     public void openConnection(InetAddress address, int port) throws IOException {
-        status.setText("Opening a connection...");
+        statusText.setText("Opening a connection...");
 
         connection = new ObjectConnection(Sources.fromSocket(address, port));
 
-        status.setText("Successfully established a connection!");
+        statusText.setText("Successfully established a connection!");
     }
 
     @FXML
     public void next() {
+        loop.setRunning(false);
         timer.stop();
 
-        appState.getContext().getPeer().initConnection(connection);
+        appState.getContext().getPeer().startConnection(connection);
+    }
+
+    private class NewConnectionChangeListener implements ChangeListener<String> {
+        @Override
+        public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+            next = true;
+        }
     }
 }
